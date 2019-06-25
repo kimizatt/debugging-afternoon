86f0a2b96281969c556046b5fc7e68bd08bb90d0
Kimberly Izatt <kimizatt@Kims-MacBook-Pro.local>
2019-06-25 15:37:58 -0600

    Done Stage 2

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './StoreFront.css';
 
 class StoreFront extends Component {
+  
   render() {
+    console.log(this.props.addToCart)
     let productDisplay = this.props.products.map((element, index) => {
       return (
-        <div className="product-container" key={index}>
+        <div className="product-container" key={index} >
           <h2>{element.title}</h2>
           <img src={element.image} alt="" />
           <h2>{element.desc}</h2>
@@ -13,9 +15,11 @@ class StoreFront extends Component {
           <button onClick={() => this.props.addToCart(element)}>Purchase!</button>
         </div>
       )
+      
     })
     return (
-      <div className="storefront-container">
+      <div className="storefront-container" >
+        {productDisplay}
       </div>
     )
   }
